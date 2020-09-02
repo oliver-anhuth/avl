698cc131bb54b94cc3046bdc8ae0b5f52f1a4dd9
Oliver Anhuth <oliver.anhuth@web.de>
2020-09-02 22:45:04 +0200

    Add benchmarks.

---
+++ bench/benches/benches.rs
@@ -0,0 +1,53 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand::{rngs::StdRng, Rng, SeedableRng};
+
+use avl::AvlTreeMap;
+
+const N: i32 = 100_000;
+
+pub fn bench(c: &mut Criterion) {
+    let mut rng = StdRng::seed_from_u64(0);
+    let values: Vec<i32> = (0..N).map(|_| rng.gen()).collect();
+
+    c.bench_function("map_insert", |b| {
+        let mut map = AvlTreeMap::new();
+        b.iter(|| {
+            for value in &values {
+                map.insert(*value, *value);
+            }
+        })
+    });
+
+    let mut map = AvlTreeMap::new();
+    for value in &values {
+        map.insert(*value, *value);
+    }
+
+    c.bench_function("map_get", |b| {
+        b.iter(|| {
+            for value in &values {
+                black_box(map.get(value));
+            }
+        })
+    });
+
+    c.bench_function("map_iter", |b| {
+        b.iter(|| {
+            for (k, v) in &map {
+                black_box((k, v));
+            }
+        })
+    });
+
+    c.bench_function("map_remove", |b| {
+        let mut map = map.clone();
+        b.iter(|| {
+            for value in &values {
+                map.remove(value);
+            }
+        })
+    });
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
