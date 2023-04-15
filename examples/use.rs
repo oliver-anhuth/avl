7e45adb764377a7ea56cd0db785c93e2c706c059
Oliver Anhuth <oliver.anhuth@web.de>
2023-04-15 22:14:12 +0200

    Extend example code.

--- examples/use.rs
+++ examples/use.rs
@@ -5,15 +5,29 @@ fn main() {
     map.insert(0, "zero");
     map.insert(1, "one");
     map.insert(2, "two");
+    map.insert(2, "two");
+    map.insert(3, "three");
+    map.insert(4, "four");
+    map.insert(5, "five");
     assert_eq!(map.get(&1), Some(&"one"));
     map.remove(&1);
     assert!(map.get(&1).is_none());
 
+    for (k, v) in &map {
+        println!("{k} => {v}");
+    }
+
     let mut set = AvlTreeSet::new();
-    set.insert(format!("{}", 0));
-    set.insert(format!("{}", 1));
-    set.insert(format!("{}", 2));
-    assert!(set.contains("1"));
-    set.remove("1");
-    assert!(!set.contains("1"));
+    for x in 0..5 {
+        set.insert(x);
+    }
+    assert!(set.contains(&1));
+    set.remove(&1);
+    assert!(!set.contains(&1));
+
+    print!("{{ ");
+    for x in &set {
+        print!("{x}, ");
+    }
+    println!("}}");
 }
